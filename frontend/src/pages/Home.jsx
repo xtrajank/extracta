b6fa0b8269db627cccc27a10b0ad91090c5d73d5
xtrajank <owen.rasor8@gmal.com>
2025-05-29 18:03:31 -0600

    discard login

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FileUpload from '../components/FileUpload';
-import LoginDiv from '../components/Login';
 
 export default function Home({ setSessionId }) {
     const navigate = useNavigate();
@@ -9,7 +8,6 @@ export default function Home({ setSessionId }) {
     return (
         <>
             <h1>extracta</h1>
-            <LoginDiv/>
             <FileUpload onSuccess={ () => navigate('/columns') } setSessionId={setSessionId}/>
         </>
     );
